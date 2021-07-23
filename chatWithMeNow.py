346a0b8e4bf87735f45716f84d2b963dede1fdd6
richieyuyongpoh <86595942+richieyuyongpoh@users.noreply.github.com>
2021-07-23 23:50:11 +0800

    Update chatWithMeNow.py

--- chatWithMeNow.py
+++ chatWithMeNow.py
@@ -1,5 +1,6 @@
 
 import dill
+import spacy
 
 activateFile = 'initAIChatBotHelper.pkl'
 infile = open(activateFile,'rb')
@@ -7,5 +8,5 @@ infile = open(activateFile,'rb')
 initAIChatBotHelperModule = dill.load(infile)
 infile.close()
 
-
+nlp = spacy.load("en_core_web_sm")
 initAIChatBotHelperModule()
