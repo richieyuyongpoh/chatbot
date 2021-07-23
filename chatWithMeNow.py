ec56c0917118c2de24b18b8d23da83952b6a8001
richieyuyongpoh <86595942+richieyuyongpoh@users.noreply.github.com>
2021-07-23 22:30:39 +0800

    Add files via upload

---
+++ chatWithMeNow.py
@@ -0,0 +1,11 @@
+
+import dill
+
+activateFile = 'initAIChatBotHelper.pkl'
+infile = open(activateFile,'rb')
+
+initAIChatBotHelperModule = dill.load(infile)
+infile.close()
+
+
+initAIChatBotHelperModule()
